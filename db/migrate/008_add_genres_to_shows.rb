72975b55b1a386e26b63a1a3c08f4f81bc195c1c
Sean Langton <slangton1@gmail.com>
2019-01-22 18:56:22 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_genres_to_shows.rb
+++ db/migrate/008_add_genres_to_shows.rb
@@ -1,7 +1,5 @@
 class AddGenresToShows < ActiveRecord::Migration[4.2]
   def change
-    add_column :shows, :genre, :string
-    add_column :shows, :day, :string
-    add_column :shows, :season, :string        
+    add_column :shows, :genre, :string     
   end
 end
