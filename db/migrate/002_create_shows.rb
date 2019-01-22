1853191d702cf6bc16be89c451cfcbd51dbb1a32
Sean Langton <slangton1@gmail.com>
2019-01-22 17:38:27 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_shows.rb
+++ db/migrate/002_create_shows.rb
@@ -1,12 +1,11 @@
 # DO NOT EDIT!
 # If you need to add things to shows, you need to create a new migration file
 # and additively change the schema. Always add new migrations to change the db.
-class CreateActors < ActiveRecord::Migration[4.2]
+class CreateShows < ActiveRecord::Migration
   def change
-    create_table :actors do |t|
+    create_table :shows do |t|
       #primary key of :id is created for us!
-      t.string :first_name
-      t.string :last_name
+      t.string :name
     end
   end
 end
