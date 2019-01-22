2460467d5c8fa40e8de5c4fa60d7838b4bea17b7
Sean Langton <slangton1@gmail.com>
2019-01-22 17:48:00 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrases_to_character.rb
+++ db/migrate/006_add_catchphrases_to_character.rb
@@ -1,4 +1,4 @@
-class AddShowsToNetwork < ActiveRecord::Migration[4.2]
+class AddCatchphrasesToCharacter < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :catchphrase, :string
   end
